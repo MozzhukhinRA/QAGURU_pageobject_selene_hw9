aed407bc29817b492fc2dbb021de1a60e1ae905e
MozzhukhinRA <roma.moz@mail.ru>
2025-08-22 14:23:27 +0300

    update Class in dataclass

--- QAGURU_pageobject_selene_hw9/for_high_level/user.py
+++ QAGURU_pageobject_selene_hw9/for_high_level/user.py
@@ -1,41 +1,22 @@
 import random
+from dataclasses import dataclass
 
 
-class UserPage:
-   @staticmethod
-   def generate_data_users():
-        first_name = random.choice(["Max", "Pavel", "Alex", "Skot"])
-        last_name = random.choice(["Kuznetsov", "Alekin", "Stepanov", "Pierro"])
-        full_name = f'{first_name} {last_name}'
-        moth = random.choice(["April","February", "May", "September"])
-        year = random.randint(1950, 2025)
-
-
-        mail = f'{first_name.lower()}.{last_name.lower()}@gmail.com'
-        return  {"first_name" : first_name,
-        "last_name" : last_name,
-        "full_name" : full_name,
-        "moth" : moth,
-        "year" : year,
-        "mail" : mail,
-        "gender" : random.choice(["Male", "Female", "Other"]),
-        "phone" : f'9{random.randint(000000000, 999999999)}',
-        "birth" : f'15 {moth} {year}',
-        "sub" : f'{random.choice(["English", "Physics"])}',
-        "hobbies" : f'{random.choice(["Sports", "Music"])}',
-        "picture" : 'photo.jpeg',
-        "address" : f'{random.choice(["Piter", "Ryazan"])}',
-        "region" : 'Uttar Pradesh Merrut'
-        }
-
-
-
-
-
-
-
-
-
-
-
-
+@dataclass
+class User:
+    first_name = random.choice(["Max", "Pavel", "Alex", "Skot"])
+    last_name = random.choice(["Kuznetsov", "Alekin", "Stepanov", "Pierro"])
+    mail = f'{first_name.lower()}.{last_name.lower()}@gmail.com'
+    day = random.choice(['10','9', '19', '28'])
+    moth = random.choice(["April", "February", "May", "September"])
+    year = random.randint(1950, 2025)
+    gender = random.choice(["Male", "Female", "Other"])
+    phone = f'9{random.randint(000000000, 999999999)}'
+    sub = random.choice(["English", "Physics"])
+    hobbies = random.choice(["Sports", "Music"])
+    picture = 'photo.jpeg'
+    address = random.choice(["Piter", "Ryazan"])
+    region = 'Uttar Pradesh Merrut'
+    full_name = f'{first_name} {last_name}'
+    birth_day = f'{day} {moth},{year}'
+    print(full_name)
