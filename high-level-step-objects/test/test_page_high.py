ccb0ac99f2367185fb7cc7128331cada6828bcb1
MozzhukhinRA <roma.moz@mail.ru>
2025-08-22 14:23:14 +0300

    update func for assert from dataclass

--- high-level-step-objects/test/test_page_high.py
+++ high-level-step-objects/test/test_page_high.py
@@ -1,40 +1,42 @@
+from QAGURU_pageobject_selene_hw9.for_high_level.validate_form import FormValidator
 from QAGURU_pageobject_selene_hw9.for_high_level.open_html_path_high import PhotoPage, BrowserPage, RegistrationPage
-from QAGURU_pageobject_selene_hw9.for_high_level.user import UserPage
+from QAGURU_pageobject_selene_hw9.for_high_level.user import User
 
 
 def test_requirement_form():
     photo_for_import = PhotoPage()
     personal_data = BrowserPage()
     get_send = RegistrationPage()
-    user = UserPage().generate_data_users()
+    user = User()
+    form = FormValidator
 
     # заполнение личных данных
-    personal_data.students_registration_first_name(user['first_name'])
-    personal_data.students_registration_last_name(user['last_name'])
+    personal_data.students_registration_first_name(user.first_name)
+    personal_data.students_registration_last_name(user.last_name)
 
     # заполнение эл почты
-    personal_data.students_registration_mail(user['mail'])
+    personal_data.students_registration_mail(user.mail)
 
     # выбор чекбокса Female
-    personal_data.students_registration_gender(user['gender'])
+    personal_data.students_registration_gender(user.gender)
 
     # ввод номера телефона
-    personal_data.students_registration_phone(user['phone'])
+    personal_data.students_registration_phone(user.phone)
 
     # ввод даты
-    personal_data.students_registration_date_of_birth(user['year'], user['moth'])
+    personal_data.students_registration_date_of_birth(user.year,user.moth,user.day)
 
     # заполнение области subject
-    personal_data.students_registration_subject(user['sub'])
+    personal_data.students_registration_subject(user.sub)
 
     # выбор чекбокса Hobbies
-    personal_data.students_registration_checkbox(user['hobbies'])
+    personal_data.students_registration_checkbox(user.hobbies)
 
     # загрузка файла
-    personal_data.students_registration_photo(photo_for_import.import_file('photo.jpeg'))
+    personal_data.students_registration_photo(photo_for_import.import_file(user.picture))
 
     # заполнение Current Address
-    personal_data.students_registration_address(user['address'])
+    personal_data.students_registration_address(user.address)
 
     # выбор штата
     personal_data.students_registration_state()
@@ -46,15 +48,4 @@ def test_requirement_form():
     get_send.press_submit()
 
     # проверка формы
-    get_send.assert_form(
-        user["full_name"],
-        user["mail"],
-        user["gender"],
-        user["phone"],
-        f'15 {user["moth"]},{user["year"]}',
-        user["sub"],
-        user["hobbies"],
-        user["picture"],
-        user["address"],
-        user["region"]
-    )
+    form.validate_form(user)
